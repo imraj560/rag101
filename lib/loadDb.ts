8dbbc2aee2735d518e558b910da9212ac8c08e88
imraj560 <raju560.webdev@gmail.com>
2025-09-08 11:00:29 -0400

    First Push

---
+++ lib/loadDb.ts
@@ -0,0 +1,8 @@
+import { DataAPIClient } from "@datastax/astra-db-ts";
+import { PuppeteerWebBaseLoader } from "@langchain/community/document_loaders/web/puppeteer";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { OpenAI } from "@langchain/openai";
+
+
+
+
