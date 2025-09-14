122e7c6e15c23437e72a8b03284d3c9bb5bed148
imraj560 <raju560.webdev@gmail.com>
2025-09-14 07:42:01 -0400

    Completing the vector embedding script and loaded the vector db

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1 className="text-xl font-bold underline">Hello Raju</h1>
+      <h1 className="text-xl font-bold underline">Hello Raju Lets work on your rag application</h1>
     </>
   );
 }
